8aae83aca506f7823235e47d17ff0de6b81a8df4
developersarm <abhayyadav000@gmail.com>
2018-08-27 19:25:20 +0530

    Adds some architecture

--- src/org/care/model/Member.java
+++ src/org/care/model/Member.java
@@ -1,4 +1,15 @@
 package org.care.model;
 
 public class Member {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int phoneNo;
+    private String emailId;
+    private String password;
+    private enum type {seeker, sitter}
+    private String address;
+    private enum status {active, inactive,}
+
+
 }
